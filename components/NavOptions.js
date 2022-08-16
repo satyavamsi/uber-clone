25d5f974e9e900f6452f059d05af7a6e3070a403
satyavamsi <satyavamsi222.21@gmail.com>
2022-08-16 18:15:25 +0200

    added initial setup

---
+++ components/NavOptions.js
@@ -0,0 +1,56 @@
+import { View, Text, FlatList, TouchableOpacity, Image } from "react-native";
+import React from "react";
+import tw from "tailwind-react-native-classnames";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+const IMAGE_URL = "https://links.papareact.com";
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: `${IMAGE_URL}/3pn`,
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: `${IMAGE_URL}/28w`,
+    screen: "EatsScreen",
+  },
+];
+const NavOptions = () => {
+  const navigation = useNavigation();
+
+  return (
+    <FlatList
+      scrollEnabled={false}
+      data={data}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{
+                uri: item.image,
+              }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              type="antdesign"
+              name="arrowright"
+              color="white"
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+      keyExtractor={(item) => item.id}
+    />
+  );
+};
+
+export default NavOptions;
